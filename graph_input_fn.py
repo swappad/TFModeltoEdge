469af78e93ad08887bed095823afc4b9c7a402e7
swappad <luca.krueger@me.com>
2020-10-18 13:26:51 +0200

    files for quantization

---
+++ graph_input_fn.py
@@ -0,0 +1,38 @@
+import cv2
+import os 
+import numpy as np
+import os.path
+
+from tensorflow.keras.preprocessing.image import img_to_array
+
+calib_img_path = "./data_subset"
+
+calib_batch_size = 10
+
+def calib_input(iter):
+    
+    paths = []
+    for (path, dirname, files) in sorted(os.walk(calib_img_path)):
+        for filename in sorted(files):
+            if filename.endswith(('.jpg', '.png')):
+                paths.append(os.path.join(path, filename))
+
+    images = []
+    for index in range(0, calib_batch_size):
+        #print("Path: " + paths[(iter * calib_batch_size + index) % len(paths)])
+        img = cv2.imread(paths[(iter * calib_batch_size + index) % len(paths)], 1)
+        try:
+            img = cv2.resize(img,(512, 256))
+            images.append(img)
+#            cv2.imshow('img',img)
+        except cv2.error as e:
+            print('Invalid frame!')
+            print(e)
+
+    return {"input_1": images}
+
+def main():
+    calib_input()
+
+if __name__ =="__main__":
+    main()
---
+++ quantize.sh
@@ -0,0 +1,18 @@
+FROZEN_GRAPH=./trained_models/1210_unet.pb
+INPUT_NODES="input_1"
+OUTPUT_NODES="conv2d_8/truediv"
+OUTPUT_DIR="./quantize_result"
+
+vai_q_tensorflow quantize \
+	--input_frozen_graph ${FROZEN_GRAPH} \
+	--input_nodes ${INPUT_NODES} \
+	--input_shapes ?,256,512,3 \
+	--output_nodes ${OUTPUT_NODES} \
+	--input_fn graph_input_fn.calib_input \
+	--weight_bit 8 \
+	--activation_bit 8 \
+	--method 1 \
+	--calib_iter 10 \
+	--simulate_dpu 1 \
+	--output_dir ${OUTPUT_DIR}
+
