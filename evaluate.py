3b4bfd8241d9d8862a12c41539bfdf05a58a288c
Luca Krueger <luca.krueger@me.com>
2020-11-12 11:07:18 +0000

    added script for evaluation of any frozen/quantised graph

--- evaluate.py
+++ evaluate.py
@@ -43,39 +43,45 @@ def graph_eval(input_graph_def, input_node, output_node):
     images = []
     for index in range(0, calib_batch_size):
       #print("Path: " + paths[(iter * calib_batch_size + index) % len(paths)])
-      img = cv2.imread(paths[(10 * calib_batch_size + index) % len(paths)], 1)
+      img = cv2.imread(paths[(18 * calib_batch_size + index) % len(paths)], 1)
       try:
           img = cv2.resize(img,(512, 256))
-          #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-          #img = img / 127.5 - 1
+          img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+          img = img / 127.5 - 1
           images.append(img)
+          #cv2.imwrite('input_eval_img.png', img)
     #            cv2.imshow('img',img)
       except cv2.error as e:
           print('Invalid frame!')
           print(e)
 
-    ## import and run graph
-    tf.import_graph_def(input_graph_def, name = '')
-
-    # get placeholders and tensors
-    x = tf.get_default_graph().get_tensor_by_name(input_node+':0')
-    labels = tf.placeholder(tf.int32, shape = [None, 4])
-
-    # get output tensors
-    y = tf.get_default_graph().get_tensor_by_name(output_node+':0')
 
 
     with tf.Session() as sess:
+        ## import and run graph
+        tf.import_graph_def(input_graph_def, name = '')
+
+        # get placeholders and tensors
+        for i in tf.get_default_graph().get_operations():
+            print(i.name)
+        x = tf.get_default_graph().get_tensor_by_name(input_node+':0')
+        # labels = tf.placeholder(tf.int32, shape = [None, 4])
+
+        # get output tensors
+        y = tf.get_default_graph().get_tensor_by_name(output_node+':0')
+
         sess.run(tf.initializers.global_variables())
 
         feed_dict ={x: images}
         y_pred = sess.run(y, feed_dict)
-        y = np.reshape(y_pred, (256, 512, 4))
         print(y_pred.shape)
-        print(y.shape)
         print(y_pred.dtype)
-        print(y.astype(np.uint32))
-        image = result_map_to_img(y)
+        y_out = np.reshape(y_pred.astype(np.uint8), (256, 512, 4))
+        print(y_out.shape)
+        print(y_pred)
+        image = result_map_to_img(y_pred)
+        #image = result_map_to_img(y_out.astype(np.uint8))
+        #image = result_map_to_img(y_pred.astype(np.uint8))
         cv2.imwrite('eval_img.png', image)
 
 
@@ -91,13 +97,13 @@ def main(unused_argv):
 if __name__ ==  "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--graph', type=str,
-                        default='./quantize_result/quantize_eval_model.pb',
+                        default='./trained_models/1210_unet_epoch_21/frozen_graph.pb',
                         help='graph file (.pb) to be evaluated.')
     parser.add_argument('--input_node', type=str,
                         default='input_1',
                         help='input node.')
     parser.add_argument('--output_node', type=str,
-                        default='conv2d_8/Conv2D',
+                        default='conv2d_9/BiasAdd',
                         help='output node.')
     parser.add_argument('--class_num', type=int,
                         default=4,
