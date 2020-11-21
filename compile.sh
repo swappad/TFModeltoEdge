e5247e3cbb20c56c166a5941ecd8bee5d61abe5b
Luca Krueger <luca.krueger@me.com>
2020-11-21 12:22:17 +0000

    moved Ultra.json hw config file to subfolders and changed HW_FILES path for compilation accordingly

--- compile.sh
+++ compile.sh
@@ -1,9 +1,10 @@
+HW_FILES='./hw_files/B2304'
 OUTPUT_DIR='./compile_result'
 KERNEL_NAME='Unet'
 
 vai_c_tensorflow \
 	--frozen_pb ./quantize_result/deploy_model.pb \
-	--arch ./Ultra96.json \
+	--arch ${HW_FILES}/Ultra96.json \
 	--output_dir ${OUTPUT_DIR} \
 	--net_name ${KERNEL_NAME} \
 	--options "{'mode':'normal','dump':'fused_graph_info','save_kernel':'$OUTPUT_DIR'}"
