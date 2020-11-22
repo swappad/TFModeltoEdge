736d8f3ad09646ff9c9c0d25af02a9b2072a0f34
Luca Krueger <luca.krueger@me.com>
2020-11-22 17:27:26 +0000

    changed path to dataset

--- graph_input_fn.py
+++ graph_input_fn.py
@@ -5,7 +5,7 @@ import os.path
 
 from tensorflow.keras.preprocessing.image import img_to_array
 
-calib_img_path = "./dataset/leftImg8bit_trainvaltest/leftImg8bit/train"
+calib_img_path = "./dataset/leftImg8bit/"
 
 calib_batch_size = 10
 
