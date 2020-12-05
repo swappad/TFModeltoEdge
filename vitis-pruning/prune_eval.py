d639b12dd1c040bd71e645fdef9165ee4ed0eb9e
swappad <luca.krueger@me.com>
2020-12-05 11:52:12 +0100

    fixed pruning helpers; successfully tested on keras.model.evaluate, but not inside the vitis optimizer environment due to lack of appropriate gpu

--- vitis-pruning/prune_eval.py
+++ vitis-pruning/prune_eval.py
@@ -4,47 +4,41 @@ import argparse
 import shutil
 import tensorflow as tf
 from tensorflow import keras
-import cv2
 
-from unet import unet
+from models.unet import unet
 from dataset_parser.generator import data_generator
 
-tf.compat.v1.enable_eager_execution(
-    config=None, device_policy=None, execution_mode=None
-)
+#tf.compat.v1.enable_eager_execution(config=None, device_policy=None, execution_mode=None)
 
 MODEL_WEIGHTS='../trained_models/unet_model_next.h5'
-BATCH_SIZE=10
+BATCH_SIZE=1
 
 def model_fn():
     model = unet(input_shape=(256, 512, 3), num_classes=4,
                    lr_init=1e-4, lr_decay=5e-4, vgg_weight_path=None)
-    model.load_weights()
+    model.load_weights(MODEL_WEIGHTS)
 
     estimator = tf.keras.estimator.model_to_estimator(
             keras_model=model,
-            checkpoint_format=saver)
+            checkpoint_format='checkpoint')
     return estimator
 
 def eval_input_fn():
-    # for x, y in data_generator('dataset_parser/data.h5', BATCH_SIZE, 'val'):
-    #     print(x.dtype, y.dtype)
     dataset = tf.data.Dataset.from_generator(
             lambda: data_generator('dataset_parser/data.h5', BATCH_SIZE, 'val'),
-            (tf.float32, tf.float64))
+            (tf.float32, tf.float64),
+            ((BATCH_SIZE, 256, 512, 3), (BATCH_SIZE, 256, 512, 4)))
 
-    data = dataset.batch(1)
-    iterator = data.make_one_shot_iterator()
-    x, y = iterator.get_next()
+    dataset = dataset.map(lambda features, labels: ({'input_1': tf.squeeze(features,0)}, tf.squeeze(labels,0)))
+    data = dataset.batch(2).repeat()
+#  
+    return data
 
-    return tf.estimator.inputs.numpy_input_fn(
-            x={"x": tf.squeeze(x,0)},
-            y=tf.squeeze(y,0),
-            num_epochs=1,
-            shuffle=False)
+#    return tf.compat.v1.estimator.inputs.numpy_input_fn(
+#            x={"x": x},
+#            y=tf.squeeze(y,0),
+#            num_epochs=1,
+#            shuffle=False)
 
 
-
-
-eval_input_fn()
 
