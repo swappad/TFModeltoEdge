40d99bb480f9ae61c94c950af46047360515f072
swappad <luca.krueger@me.com>
2020-12-07 20:50:43 +0100

    changed scripts back to run for unet_model_next weights

--- freeze.sh
+++ freeze.sh
@@ -1,8 +1,10 @@
-MODEL_PATH=./trained_models/unet_model_optimized_epoch_3
+MODEL_PATH=./trained_models/unet_model_next
+OUTPUT_NAME=conv2d_13/BiasAdd
+
 freeze_graph \
 	--input_graph 		${MODEL_PATH}/infer_graph.pb \
 	--input_checkpoint 	${MODEL_PATH}/float_model.ckpt \
 	--output_graph 		${MODEL_PATH}/frozen_graph.pb \
-	--output_node_names separable_conv2d_13/BiasAdd \
+	--output_node_names ${OUTPUT_NAME} \
  	--input_binary 		true
 	
--- h5topb.py
+++ h5topb.py
@@ -1,8 +1,7 @@
 import argparse
-import cv2
 import os
 import tensorflow as tf
-from unet_optimized import unet
+from models.unet import unet
 import keras as keras
 
 
@@ -12,43 +11,42 @@ parser = argparse.ArgumentParser(description='convert unet Model with h5 encoded
 parser.add_argument("--weights", help="path to h5 encoded weights")
 args = parser.parse_args()
 
-def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=False):
-    """
-    Freezes the state of a session into a pruned computation graph.
-    Creates a new computation graph where variable nodes are replaced by
-    constants taking their current value in the session. The new graph will be
-    pruned so subgraphs that are not necessary to compute the requested
-    outputs are removed.
-    @param session The TensorFlow session to be frozen.
-    @param keep_var_names A list of variable names that should not be frozen,
-                          or None to freeze all the variables in the graph.
-    @param output_names Names of the relevant graph outputs.
-    @param clear_devices Remove the device directives from the graph for better portability.
-    @return The frozen graph definition.
-    """
-    graph = session.graph
-    with graph.as_default():
-        freeze_var_names = list(set(v.op.name for v in tf.compat.v1.global_variables()).difference(keep_var_names or []))
-        output_names = output_names or []
-        output_names += [v.op.name for v in tf.compat.v1.global_variables()]
-        input_graph_def = graph.as_graph_def()
-        if clear_devices:
-            for node in input_graph_def.node:
-                node.device = ""
-#        input_graph_def = tf.compat.v1.graph_util.remove_training_nodes(input_graph_def, protected_nodes=None)
-        frozen_graph = tf.compat.v1.graph_util.convert_variables_to_constants(
-                session, input_graph_def, output_names, freeze_var_names)
-    return frozen_graph
+#def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=False):
+#    """
+#    Freezes the state of a session into a pruned computation graph.
+#    Creates a new computation graph where variable nodes are replaced by
+#    constants taking their current value in the session. The new graph will be
+#    pruned so subgraphs that are not necessary to compute the requested
+#    outputs are removed.
+#    @param session The TensorFlow session to be frozen.
+#    @param keep_var_names A list of variable names that should not be frozen,
+#                          or None to freeze all the variables in the graph.
+#    @param output_names Names of the relevant graph outputs.
+#    @param clear_devices Remove the device directives from the graph for better portability.
+#    @return The frozen graph definition.
+#    """
+#    graph = session.graph
+#    with graph.as_default():
+#        freeze_var_names = list(set(v.op.name for v in tf.compat.v1.global_variables()).difference(keep_var_names or []))
+#        output_names = output_names or []
+#        output_names += [v.op.name for v in tf.compat.v1.global_variables()]
+#        input_graph_def = graph.as_graph_def()
+#        if clear_devices:
+#            for node in input_graph_def.node:
+#                node.device = ""
+##        input_graph_def = tf.compat.v1.graph_util.remove_training_nodes(input_graph_def, protected_nodes=None)
+#        frozen_graph = tf.compat.v1.graph_util.convert_variables_to_constants(
+#                session, input_graph_def, output_names, freeze_var_names)
+#    return frozen_graph
 
 
 keras.backend.clear_session()
 keras.backend.set_learning_phase(0)
 with keras.backend.get_session() as sess:
+#with tf.compat.v1.Session() as sess:
 
     model = unet(input_shape=(256, 512, 3), num_classes=4, lr_init=1e-3, lr_decay=5e-4)
     model.load_weights(args.weights)
-    directory = './trained_models/'
-    filename = '1210_unet'
 
 #    # save complete model for comparison
 #    print(model.inputs)
@@ -56,11 +54,11 @@ with keras.backend.get_session() as sess:
 
     # save frozen subset graph for acceleration
     output_graph = os.path.splitext(args.weights)[0] #+ ".pb"
-    output_names= ['separable_conv2d_13/BiasAdd']
+    # output_names= ['separable_conv2d_13/BiasAdd']
 
     input_names=[out.op.name for out in model.inputs]
     print(input_names)
-    # output_names=[out.op.name for out in model.outputs]
+    output_names=[out.op.name for out in model.outputs]
     print('input  node is{}'.format(input_names))
     print('output node is{}'.format(output_names))
 
