33c32a47eb71db0b72b034f5ace6f68c46043a22
swappad <luca.krueger@me.com>
2020-12-08 02:27:35 +0100

    removed old files

--- dump.sh
+++
@@ -1,7 +0,0 @@
-vai_q_tensorflow dump \
-	--input_frozen_graph ./quantize_result/quantize_eval_model.pb \
-	--input_fn graph_input_fn.calib_input \
-	--max_dump_batches 1 \
-	--dump_float 0 \
-	--output_dir quantize_result \
-
